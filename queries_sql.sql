a294577169925bde219ade6f7cab23f11816cd3c
Joshua Katz <jakatz87@gmail.com>
2022-11-13 19:19:13 -0500

    Adding sql queries.

---
+++ queries_sql.sql
@@ -0,0 +1,30 @@
+-- Search for entries based on birthday for retirment age
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Search by each year in the range
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility based on hiring date
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+	AND (hire_date BETWEEN '1985-01-01'AND '1988-12-31');
+	
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Create a new table based on query
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+	AND (hire_date BETWEEN '1985-01-01'AND '1988-12-31');
+
+SELECT * FROM retirement_info;
